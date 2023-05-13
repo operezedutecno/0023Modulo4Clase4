e55692c0d1047891ff3c7251cdaa64fa7fa10232
Osman Pérez <operez@edutecno.com>
2023-05-12 20:27:26 -0400

    Ejemplos de MAP y SET

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,15 @@ const producto = {
     tipo: 'Harinas'
 }
 
+const otraInfoProducto = {
+    ingredientes: [
+        "Germen de trigo",
+        "Mejoradores de masa"
+    ],
+    vitaminas: ["B6","B12"],
+    peso: 2000
+}
+
 var tipo = "Pagina Web"
 
 //Opción 1 para obtener valores de OBJETOS
@@ -66,5 +75,71 @@ const apellidos = ["Pérez", "Gonzalez","Ojeda"]
 //Opción2 para concatenar arreglos
 const nuevosNombres = [...nombres, ...otrosNombres, ...apellidos]
 
-console.log(nuevosNombres);
+
+// console.log(producto);
+// console.log(otraInfoProducto);
+
+
+// Opción 1 para concatenar atributos de objetos
+const producto2 = JSON.parse(JSON.stringify(producto))
+// const producto2 = { ...producto }
+for (const key of Object.keys(otraInfoProducto)) {
+    producto2[key] = otraInfoProducto[key]
+}
+
+// Opción 2 para concatenar atributos de objetos (Operador de propagación - Operator Spread)
+// const producto2 = {...otraInfoProducto, ...producto };
+// console.log("Producto2", producto2);
+// console.log("Producto", producto);
+
+
+
+// Clase Set - Crear objeto con valores únicos ******************************************
+const numeros = new Set([2,2,3,3,5,5,8,5,2])
+
+//Función Add añade elementos al SET
+numeros.add("3")
+numeros.add(9)
+
+//Atributo size nos muestra la longitud del elemento SET
+console.log(numeros);
+console.log("longitud", numeros.size );
+
+/* Función delete elimina el elemento del SET en caso de que exista, 
+    y nos retorna un boolean para comprobar si eliminó un elemento */
+const confirmacionEliminacion = numeros.delete(1)
+console.log("confirmacionEliminacion", confirmacionEliminacion);
+
+
+// Función values obtiene cada uno de los elementos del SET
+console.log("Values", numeros.values());
+
+// Función Has nos permite saber si el valor existe en el SET, retorna un boolean
+console.log("Has", numeros.has(2));
+
+//Nos permite limpiar la estructura SET
+numeros.clear()
+
+console.log(numeros);
+
+
+// Clase Map *******************************************************************
+const personas = new Map()
+
+// La función SET incorpora un nuevo elemento al MAP
+personas.set('maria', { rut: '12345678-5', nombre: "María González", edad: 28 })
+personas.set('jose', { rut: '12345699-5', nombre: "José González", edad: 35 })
+
+// Si se repite la llave, sustituye la definición anterior
+personas.set('jose', { rut: '99999999-5', nombre: "José Mora", edad: 38 })
+
+// La función delete elimina un elemento del MAP y retorna un boolean para comprobar si hubo eliminación
+const eliminoPersona = personas.delete("jose")
+
+
+// La función Has nos devuelve un boolean para comprabar si existe un elemento con la llave proporcionada
+console.log("has", personas.has("maria"));
+
+console.log(personas);
+console.log("Eliminó?", eliminoPersona);
 
